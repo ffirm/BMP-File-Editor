aa64a871ed048c10ded1c490490ae52e094b350c
Thanat Suppagornmongkol <firm.thanat@gmail.com>
2021-11-07 08:53:25 +0700

    Project setup

---
+++ bmp-editor/cmake-build-debug/CMakeFiles/bmp_editor.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bmp_editor.dir/main.c.obj"
+  "bmp_editor.exe"
+  "bmp_editor.exe.manifest"
+  "bmp_editor.pdb"
+  "libbmp_editor.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/bmp_editor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
