479fdbf0ac04b5b84ca48d42fb6e9a6aefe2d5d5
Thanat Suppagornmongkol <firm.thanat@gmail.com>
2021-11-26 13:40:18 +0700

    UPDATE read_bmp()
    read_bmp() can now read file information from header but reading from pixel array is still not done

--- bmp-editor/main.c
+++ bmp-editor/main.c
@@ -11,48 +11,73 @@
 #define BUFFER_SIZE 1024
 
 typedef unsigned int int32;
+typedef unsigned char byte;
 
-unsigned int row_size();
-void read_bmp();
+typedef struct colours {
+    float r, g, b;
+} colours;
+
+typedef struct bmp_info {
+    int32 width;
+    int32 height;
+    int32 bpp;
+    int32 image_size;
+    int32 array_offset;
+    byte pixel;
+} bmp_info;
+
+void read_bmp(char* path, bmp_info *infoPtr);
 
 int main() {
-    read_bmp();
+    bmp_info *infoPtr, pic1;
+    infoPtr = &pic1;
+    read_bmp("../sample.bmp", infoPtr);
     return 0;
 }
 
-void read_bmp(){
-    FILE *fp = fopen("../sample.bmp", "rb");
-    if (fp == NULL){
+void read_bmp(char* path, bmp_info *infoPtr) {
+    FILE *fp = fopen(path, "rb");
+    if (fp == NULL) {
         printf("Error opening file");
         fclose(fp);
         exit(1);
     }
-    int32 data, height, width, bits_per_pixel, header_size, image_size, pixel_array_offset, temp;
-    char compression_method[BUFFER_SIZE];
-    fseek(fp, PIXEL_ARRAY_OFFSET, SEEK_SET);
-    fread(&data, 4, 1, fp);
-    fseek(fp, WIDTH_OFFSET, SEEK_SET);
-    fread(&width, 4, 1, fp);
-    fseek(fp, HEIGHT_OFFSET, SEEK_SET);
-    fread(&height, 4, 1, fp);
-    fseek(fp, BITS_PER_PIXEL_OFFSET, SEEK_SET);
-    fread(&bits_per_pixel, 2, 1, fp);
-    fseek(fp, HEADER_SIZE_OFFSET, SEEK_SET);
-    fread(&header_size, 4, 1, fp);
-    fseek(fp, IMAGE_SIZE_OFFSET, SEEK_SET);
-    fread(&image_size, 4, 1, fp);
-    fseek(fp, PIXEL_ARRAY_OFFSET, SEEK_SET);
-    fread(&pixel_array_offset, 4, 1, fp);
-    fseek(fp, 0xCA, SEEK_SET);
-    fread(&temp, 4, 1, fp);
-    printf("height: %d\n", height);
-    printf("width: %d\n", width);
-    printf("bits per pixel: %d\n", bits_per_pixel);
-    printf("header size: %d bytes\n", header_size);
-    printf("image size: %d bytes\n", image_size);
-    printf("pixel arrays starts at: %X\n", pixel_array_offset);
-    printf("%d", temp);
-}
 
+    // Reading basic file information
+    fseek(fp, WIDTH_OFFSET, SEEK_SET);
+    fread(&infoPtr->width, 4, 1, fp);
+    fseek(fp, HEIGHT_OFFSET, SEEK_SET);
+    fread(&infoPtr->height, 4, 1, fp);
+    fseek(fp, BITS_PER_PIXEL_OFFSET, SEEK_SET);
+    fread(&infoPtr->bpp, 2, 1, fp);
+    fseek(fp, IMAGE_SIZE_OFFSET, SEEK_SET);
+    fread(&infoPtr->image_size, 4, 1, fp);
+    fseek(fp, PIXEL_ARRAY_OFFSET, SEEK_SET);
+    fread(&infoPtr->array_offset, 4, 1, fp);
+
+    // Reading pixel array
+    const int padding_amount = ((4 - (infoPtr->width * 3) % 4) % 4);
+    colours pixels[infoPtr->width * infoPtr-> height];
+
+    for (int y = 0; y < infoPtr->width; y++){
+        for (int x = 0; x < infoPtr->height; x++){
+            byte colour[3];
+            fread(colour, 3, 1, (void*)infoPtr->array_offset);
+
+            pixels[y * infoPtr->width + x].r = (float)colour[3] / 255.0f;
+            pixels[y * infoPtr->width + x].g = (float)colour[3] / 255.0f;
+            pixels[y * infoPtr->width + x].b = (float)colour[3] / 255.0f;
+        }
+    }
+
+    fclose(fp);
+
+    printf("path: %s\n", path);
+    printf("image size: %d bytes\n", infoPtr->image_size);
+    printf("height: %d\n", infoPtr->height);
+    printf("width: %d\n", infoPtr->width);
+    printf("bits per pixel: %d\n", infoPtr->bpp);
+    printf("pixel arrays starts at: %d (0x%X)\n", infoPtr->array_offset, infoPtr->array_offset);
+}
 
 
