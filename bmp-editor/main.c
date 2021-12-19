d9b126ec219d8e12ab96a02242438bd100d8b90e
Thanat Suppagornmongkol <firm.thanat@gmail.com>
2021-12-19 10:16:25 +0700

    Add new pics, read files
    Added 2 new pics and read_bmp() reads the file basic information and store the pixel array into a structure.

--- bmp-editor/main.c
+++ bmp-editor/main.c
@@ -1,21 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <string.h>
 
 #define PIXEL_ARRAY_OFFSET 0x000A
 #define WIDTH_OFFSET 0x12
 #define HEIGHT_OFFSET 0x16
 #define BITS_PER_PIXEL_OFFSET 0x1C
-#define HEADER_SIZE_OFFSET 0x0E
 #define IMAGE_SIZE_OFFSET 0x22
-#define COMPRESSION_METHOD_OFFSET 0x30
-#define BUFFER_SIZE 1024
 
 typedef unsigned int int32;
 typedef unsigned char byte;
-
-typedef struct colours {
-    float r, g, b;
-} colours;
 
 typedef struct bmp_info {
     int32 width;
@@ -23,15 +18,29 @@ typedef struct bmp_info {
     int32 bpp;
     int32 image_size;
     int32 array_offset;
-    byte pixel;
+    byte *pixel_array;
 } bmp_info;
 
-void read_bmp(char* path, bmp_info *infoPtr);
+void slice_str(const char *str, char *buffer, size_t start, size_t end);
+void read_bmp(char *path, bmp_info *infoPtr);
+void merge_bmp();
 
 int main() {
-    bmp_info *infoPtr, pic1;
-    infoPtr = &pic1;
-    read_bmp("../sample.bmp", infoPtr);
+//    bmp_info *infoPtr, pic1;
+//    infoPtr = &pic1;
+//    read_bmp("../sample.bmp", infoPtr);
+
+    bmp_info *greenland, pic2;
+    greenland = &pic2;
+    bmp_info *deer, pic3;
+    deer = &pic3;
+    
+    read_bmp("../greenland_grid_velo.bmp", greenland);
+    read_bmp("../blackbuck.bmp", deer);
+
+    free(greenland);
+    free(deer);
+
     return 0;
 }
 
@@ -42,6 +51,20 @@ void read_bmp(char* path, bmp_info *infoPtr) {
         fclose(fp);
         exit(1);
     }
+
+    // Check if file is BMP or not
+    byte file_sig[3];
+    file_sig[2] = '\0';
+    fseek(fp, 0, SEEK_SET);
+    fread(file_sig, 2, 1, fp);
+    if (strcmp("BM", file_sig) != 0){
+        printf("Invalid BMP file");
+        exit(1);
+    }
+
+    // file size
+    fseek(fp, 0, SEEK_END);
+    size_t f_size = ftell(fp);
 
     // Reading basic file information
     fseek(fp, WIDTH_OFFSET, SEEK_SET);
@@ -55,29 +78,27 @@ void read_bmp(char* path, bmp_info *infoPtr) {
     fseek(fp, PIXEL_ARRAY_OFFSET, SEEK_SET);
     fread(&infoPtr->array_offset, 4, 1, fp);
 
-    // Reading pixel array
-    const int padding_amount = ((4 - (infoPtr->width * 3) % 4) % 4);
-    colours pixels[infoPtr->width * infoPtr-> height];
-
-    for (int y = 0; y < infoPtr->width; y++){
-        for (int x = 0; x < infoPtr->height; x++){
-            byte colour[3];
-            fread(colour, 3, 1, (void*)infoPtr->array_offset);
-
-            pixels[y * infoPtr->width + x].r = (float)colour[3] / 255.0f;
-            pixels[y * infoPtr->width + x].g = (float)colour[3] / 255.0f;
-            pixels[y * infoPtr->width + x].b = (float)colour[3] / 255.0f;
-        }
+    // fread pixel array from file into a malloced pointer called color_array
+    infoPtr->pixel_array = (byte*)malloc(infoPtr->image_size);
+    fseek(fp, infoPtr->array_offset, SEEK_SET);
+    int res = fread(infoPtr->pixel_array, 1, infoPtr->image_size, fp);
+    if (res != infoPtr->image_size){
+        printf("fread pixel array error");
+        exit(1);
     }
 
     fclose(fp);
 
     printf("path: %s\n", path);
-    printf("image size: %d bytes\n", infoPtr->image_size);
+    printf("image size: %d bytes\n", f_size);
     printf("height: %d\n", infoPtr->height);
     printf("width: %d\n", infoPtr->width);
     printf("bits per pixel: %d\n", infoPtr->bpp);
-    printf("pixel arrays starts at: %d (0x%X)\n", infoPtr->array_offset, infoPtr->array_offset);
-}
+    printf("pixel arrays starts at: 0x%X\n", infoPtr->array_offset);
 
+    byte temp[2];
+    fseek(fp, BITS_PER_PIXEL_OFFSET, SEEK_SET);
+    fread(temp, 2, 1, fp);
+    printf("%d\n", temp);
+}
 
